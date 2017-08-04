14a8399db5f01201c0334b41d33b98d5e658434b
jeromecr <30685289+jeromecr@users.noreply.github.com>
2017-08-04 18:05:28 +0200

    Add files via upload

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$packageName = 'VeeamAgentforWindows'
+$ErrorActionPreference = 'Stop';
+
+$toolsDir   = $(Split-Path -Parent $MyInvocation.MyCommand.Definition)
+$installerDir = Join-Path $toolsDir 'installerDir'
+$downloadURL = 'https://download2.veeam.com/VeeamAgentWindows_2.0.0.700.zip'
+$installer = 'VeeamAgentWindows_2.0.0.700.exe'
+
+
+Install-ChocolateyZipPackage $packageName -url $downloadURL -UnzipLocation $installerDir
+
+$packageArgs = @{
+	packageName   = $installer 
+	fileType      = 'exe'
+	file          = "$installerDir\$installer"
+	silentArgs    = '/silent /accepteula'
+  ValidExitCodes = @(0,1000,1101)
+}
+
+Install-ChocolateyInstallPackage @packageArgs 
